c9330e20a239dedca23704dd2823e44cd1e68162
Santiago Vazquez Molina <santvm280897@gmail.com>
2020-04-16 02:23:30 -0500

    Object notes

---
+++ ObjectNotes.R
@@ -0,0 +1,18 @@
+# R Objects 
+
+# Classes: Character, numeric (real numbers), integer, complex, logical(True/false)
+
+#The most basic object is a vector 
+
+#Vector can only contain objects of the same class, except lists 
+
+#Empty vecots can be created with vector(type,length)
+
+#1 numeric 1L integer
+
+#Inf is infinity 
+
+#NaN not a number 
+
+#Objects have attributes like names, dimnames, dimensions, class, length
+#attributes can be accesed by attributes()
